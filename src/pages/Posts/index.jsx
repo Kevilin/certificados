a7a82edaeec89616837e109c314ea2e4dae3d8cc
kevilin.rodrigues <kevilin.rodrigues@kinghost.com.br>
2023-04-22 02:21:13 -0300

    Adicionando feature

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,38 +1,73 @@
-import React from "react";
-import { useEffect } from "react";
-import { Stack, Text, Grid, Box, Spinner, Center } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Stack, Text, Grid, Box, Spinner, Center, Button } from "@chakra-ui/react";
 import { nanoid } from "nanoid";
 import { GetData } from "../Home/Hosts/Host.logical";
 import Cards from "../../components/Cards";
 
 const Posts = () => {
   const { getDataFirebase, data } = GetData();
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     getDataFirebase();
     window.scrollTo({ top: 0 });
   }, []);
+
   return (
-    <Stack bg="#2E2E2E">
+    <Stack bg="#2E2E2E" >
       <Box minHeight="100vh" mb={20}>
-        <Stack direction="row" flexWrap="wrap" p={10} justifyContent="center">
+        <Stack direction="row" flexWrap="wrap" p={2} justifyContent="center">
+          <Stack direction="row" mb={4} spacing={4} p={2}>
+            <Button
+              colorScheme={selectedCategory === "" ? "brand.btn" : ""}
+              onClick={() => setSelectedCategory("")}
+            >
+              Todos
+            </Button>
+            <Button
+              colorScheme={selectedCategory === "Backend" ? "brand.btn" : ""}
+              onClick={() => setSelectedCategory("Backend")}
+            >
+              Backend
+            </Button>
+            <Button
+              colorScheme={selectedCategory === "Frontend" ? "brand.btn" : ""}
+              onClick={() => setSelectedCategory("Frontend")}
+            >
+              Frontend
+            </Button>
+            <Button
+              colorScheme={selectedCategory === "Devops" ? "brand.btn" : ""}
+              onClick={() => setSelectedCategory("Devops")}
+            >
+              Devops
+            </Button>
+            <Button
+              colorScheme={selectedCategory === "Git" ? "brand.btn" : ""}
+              onClick={() => setSelectedCategory("Git")}
+            >
+              Git
+            </Button>
+            <Button
+              colorScheme={selectedCategory === "Banco de dados" ? "brand.btn" : ""}
+              onClick={() => setSelectedCategory("Banco de dados")}
+            >
+              Banco de dados
+            </Button>
+          </Stack>
           <Grid
             templateColumns={{
               base: "repeat(1, 1fr)",
               md: "repeat(3, 1fr)",
               xl: "repeat(3, 1fr)",
             }}
-            gap={6}
+            gap={8}
           >
-            {data.length === 0 ? (
-              <Center w="100%">
-                <Spinner size="xl" color="brand.btn" m="0 auto" />
-              </Center>
-            ) : (
-              data.map((post) => {
+            {data
+              .filter((post) => selectedCategory === "" || post.data().inputs.categoria === selectedCategory)
+              .map((post) => {
                 return <Cards key={nanoid()} post={post} width="30%" />;
-              })
-            )}
+              })}
           </Grid>
         </Stack>
       </Box>
