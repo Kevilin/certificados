18eb37342774cfc55c766a32f21412a050f6e5c5
kevilin.rodrigues <kevilin.rodrigues@kinghost.com.br>
2023-04-22 01:43:27 -0300

    Adicionando feature

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { Stack, Box, Text, StackDivider } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useUserAuth } from "../../context/userAuthContext";
 
 const Footer = () => {
+  const { usuario } = useUserAuth();
+
   return (
     <>
-      <Stack flexWrap="wrap" direction="row" as="footer" alignItems="center" justifyContent="space-between" bg="brand.footerbg">
+      <Stack
+        flexWrap="wrap"
+        direction="row"
+        as="footer"
+        alignItems="center"
+        justifyContent="space-between"
+        bg="brand.footerbg"
+      >
         <StackDivider p={1} />
         <Stack direction="row" spacing={5} color="brand.btn">
           <Link to="/certificados">Inicio</Link>
-          <Link to="/certificados/adm">Admin</Link>
+          {usuario.email ? (
+            <Link to="/certificados/add">Admin</Link>
+          ) : (
+            <Link to="/certificados/adm">Admin</Link>
+          )}
         </Stack>
       </Stack>
-      <Text textAlign="center" fontSize=".9rem" p={3} bg="brand.footerbg" color="brand.bg">
+      <Text
+        textAlign="center"
+        fontSize=".9rem"
+        p={3}
+        bg="brand.footerbg"
+        color="brand.bg"
+      >
         Desenvolvido por{" "}
         <Box
           as="a"
